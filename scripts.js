05cdbdf83c6e668244c743d5e215e9b574082874
Talha <talha.b.future@gmail.com>
2022-02-20 10:49:58 +0300

    changed to click

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+const slot = (() => {
+  const slot0 = document.getElementById('0-slot');
+  const slot1 = document.getElementById('1-slot');
+  const slot2 = document.getElementById('2-slot');
+  const slot3 = document.getElementById('3-slot');
+  const slot4 = document.getElementById('4-slot');
+  const slot5 = document.getElementById('5-slot');
+  const slot6 = document.getElementById('6-slot');
+  const slot7 = document.getElementById('7-slot');
+  const slot8 = document.getElementById('8-slot');
+  return {  
+    slot0, 
+    slot1, slot2,
+    slot3, slot4,
+    slot5, slot6,
+    slot7, slot8
+  };
+})();
+
+
+let slotsArray = [slot.slot0, slot.slot1, slot.slot2, slot.slot3, slot.slot4, slot.slot5, slot.slot6, slot.slot7, slot.slot8]
+let turn = `X`
 
 //this one is the class
 const player = (name, mark) => {
@@ -13,7 +35,8 @@ const player = (name, mark) => {
 
 
   const gameBoard = (() => {
-      const sayPlayersName = (player1 , player2) => console.log(`....kplayer1 is ${player1.name} and player2 is ${player2.name} hehe`)
+      const sayPlayersName = (player1 , player2) => console.log(` player1 is ${player1.name} and player2 is ${player2.name} hehe`)
+      const playerMove = (player , location) => console.log(`this will set the location wiht the player marker`)
       return { sayPlayersName };
     })();
   
@@ -21,4 +44,29 @@ const player = (name, mark) => {
   
 
 
-    
+
+    slotsArray[0].addEventListener("click" , function(e) {
+      changeTheSlot(0);
+  }); 
+    slotsArray[1].addEventListener("click" , function(e) {
+      changeTheSlot(1);
+  }); 
+    // slotsArray[2].addEventListener("click" , changeTheSlot(2) ); 
+    // slotsArray[3].addEventListener("click" , changeTheSlot(3) ); 
+    // slotsArray[4].addEventListener("click" , changeTheSlot(4) ); 
+    // slotsArray[5].addEventListener("click" , changeTheSlot(5) ); 
+    // slotsArray[6].addEventListener("click" , changeTheSlot(6) ); 
+    // slotsArray[7].addEventListener("click" , changeTheSlot(7) ); 
+    // slotsArray[8].addEventListener("click" , changeTheSlot(8) ); 
+
+function changeTheSlot(number) {
+    console.log(`inside changeTheSlot`);
+  slotsArray[number].textContent = `${turn}`; 
+  if (turn ===`X`) {
+    turn = `O`
+  }
+  if (turn === `O`) {
+    turn = `X`
+  }
+ 
+}
