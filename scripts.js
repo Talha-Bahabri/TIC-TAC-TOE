25015ef6498a9330020563a4d7ef575ad79a39ce
Talha <talha.b.future@gmail.com>
2022-02-23 18:04:03 +0300

    game stops when someone wins

--- scripts.js
+++ scripts.js
@@ -36,21 +36,32 @@ const player = (name, mark) => {
   const gameBoard = (() => {
       const sayPlayersName = (player1 , player2) => console.log(` player1 is ${player1.name} and player2 is ${player2.name} hehe`)
      //this loop is all the event listener ( instead of making 8 , make 1 loop)
+        if (winCondition() === `No winner yet`) {
+          
+      for (let i = 0 ; i<=8 ; i++) {  
+        slotsArray[i].addEventListener("click" , function(e) {
+          console.log(winCondition())
+          changeTheSlot(i);
+        });
+       }
        
-    for (let i = 0 ; i<=8 ; i++) {  
-    slotsArray[i].addEventListener("click" , function(e) {
+      }
 
-      winCondition()
-
-      changeTheSlot(i);
-    });
-  }
-  function changeTheSlot(number) {  
-    if (slotsArray[number].textContent !== `X` && slotsArray[number].textContent !== `O`) {
-      slotsArray[number].textContent = `${turn}`; 
 
       
+    let turnPlayed = 0;
+  function changeTheSlot(number) {  
 
+    if (winCondition() !== `No winner yet`) {
+      
+      return 0;
+
+    }
+
+    turnPlayed ++ ;
+
+    if (slotsArray[number].textContent !== `X` && slotsArray[number].textContent !== `O`) {
+      slotsArray[number].textContent = `${turn}`; 
 
     //this one will change the player
     if (turn ==`${player1.mark}`) {
@@ -76,36 +87,44 @@ const player = (name, mark) => {
     gameBoard.sayPlayersName(player1,player2);
   
 let turn = `${player1.mark}`
-
  
 
 function winCondition(){ 
+
   //Side pattern For X
   if (slotsArray[0].textContent === `X` && slotsArray[1].textContent === `X` && slotsArray[2].textContent === `X` ) {
     console.log(`${player1.name} wins`)
+    return player1.name;
   }
   if (slotsArray[3].textContent === `X` && slotsArray[4].textContent === `X` && slotsArray[5].textContent === `X` ) {
     console.log(`${player1.name} wins`)
+    return player1.name;
   }
   if (slotsArray[6].textContent === `X` && slotsArray[7].textContent === `X` && slotsArray[8].textContent === `X` ) {
     console.log(`${player1.name} wins`)
+    return player1.name;
   }
 // win by horizontal X
   if (slotsArray[0].textContent === `X` && slotsArray[3].textContent === `X` && slotsArray[6].textContent === `X` ) {
     console.log(`${player1.name} wins hhh`)
+    return player1.name;
   }
   if (slotsArray[1].textContent === `X` && slotsArray[4].textContent === `X` && slotsArray[7].textContent === `X` ) {
     console.log(`${player1.name} wins hhh`)
+    return player1.name;
   }
   if (slotsArray[2].textContent === `X` && slotsArray[5].textContent === `X` && slotsArray[8].textContent === `X` ) {
     console.log(`${player1.name} wins hhh`)
+    return player1.name;
   }
 // win by X cross
   if (slotsArray[0].textContent === `X` && slotsArray[4].textContent === `X` && slotsArray[8].textContent === `X` ) {
     console.log(`${player1.name} wins cross`)
+    return player1.name;
   }
   if (slotsArray[2].textContent === `X` && slotsArray[4].textContent === `X` && slotsArray[6].textContent === `X` ) {
     console.log(`${player1.name} wins`)
+    return player1.name;
   }
    
 
@@ -114,29 +133,39 @@ function winCondition(){
   //Side pattern For O
   if (slotsArray[0].textContent === `O` && slotsArray[1].textContent === `O` && slotsArray[2].textContent === `O` ) {
     console.log(`${player2.name} wins`)
+    return player2.name;
   }
   if (slotsArray[3].textContent === `O` && slotsArray[4].textContent === `O` && slotsArray[5].textContent === `O` ) {
     console.log(`${player2.name} wins`)
+    return player2.name;
   }
   if (slotsArray[6].textContent === `O` && slotsArray[7].textContent === `O` && slotsArray[8].textContent === `O` ) {
     console.log(`${player2.name} wins`)
+    return player2.name;
   }
 // win by horizontal O
   if (slotsArray[0].textContent === `O` && slotsArray[3].textContent === `O` && slotsArray[6].textContent === `O` ) {
     console.log(`${player2.name} wins hhh`)
+    return player2.name;
   }
   if (slotsArray[1].textContent === `O` && slotsArray[4].textContent === `O` && slotsArray[7].textContent === `O` ) {
     console.log(`${player2.name} wins hhh`)
+    return player2.name;
   }
   if (slotsArray[2].textContent === `O` && slotsArray[5].textContent === `O` && slotsArray[8].textContent === `O` ) {
     console.log(`${player2.name} wins hhh`)
+    return player2.name;
   }
 // win by O cross
   if (slotsArray[0].textContent === `O` && slotsArray[4].textContent === `O` && slotsArray[8].textContent === `O` ) {
     console.log(`${player2.name} wins cross`)
+    return player2.name;
   }
   if (slotsArray[2].textContent === `O` && slotsArray[4].textContent === `O` && slotsArray[6].textContent === `O` ) {
     console.log(`${player2.name} wins`)
+    return player2.name;
   }
+
+ return `No winner yet` ;
   
 }
